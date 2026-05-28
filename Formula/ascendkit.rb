371708ab25f3874126a28b8556de4f0c863ee6b2
Aben <me@aben.io>
2026-05-28 22:55:06 +0800

    Update ascendkit to v1.5.2

--- Formula/ascendkit.rb
+++ Formula/ascendkit.rb
@@ -1,8 +1,8 @@
 class Ascendkit < Formula
   desc "Local-first App Store release preparation toolkit"
   homepage "https://github.com/rushairer/AscendKit"
-  url "https://github.com/rushairer/AscendKit/releases/download/v1.5.0/ascendkit-1.5.0-macos-universal.tar.gz"
-  sha256 "3d1bb0fad7bd1f45b240fa45ef7094715040554bf1dbf64a510b342f5dd72c4a"
+  url "https://github.com/rushairer/AscendKit/releases/download/v1.5.2/ascendkit-1.5.2-macos-universal.tar.gz"
+  sha256 "a39b84b83648c00f770740a27f66f7e7823433a3eed669ae832b71db3227e697"
   license "MIT"
 
   def install
