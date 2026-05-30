2e248d0a5ba813f351c8075075049c93dd474f47
Aben <me@aben.io>
2026-05-30 10:21:21 +0800

    chore: bump to 1.6.1

--- Formula/ascendkit.rb
+++ Formula/ascendkit.rb
@@ -1,8 +1,8 @@
 class Ascendkit < Formula
   desc "Local-first App Store release preparation toolkit"
   homepage "https://github.com/rushairer/AscendKit"
-  url "https://github.com/rushairer/AscendKit/releases/download/v1.5.2/ascendkit-1.5.2-macos-universal.tar.gz"
-  sha256 "a39b84b83648c00f770740a27f66f7e7823433a3eed669ae832b71db3227e697"
+  url "https://github.com/rushairer/AscendKit/releases/download/v1.6.1/ascendkit-1.6.1-macos-universal.tar.gz"
+  sha256 "73b09c686278bd76b865df32de1f60ee7dff20e260d8d07b8453dbd0496fce0f"
   license "MIT"
 
   def install
